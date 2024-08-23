fbd696fa498fda886d47e119b2ea9be4effe1abf
大島　慶之 <160297931+jillsaw@users.noreply.github.com>
2024-08-23 21:22:48 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+//変数宣言
+let bodyWeght
+//値の代入
+bodyWeght=68
+//変数宣言
+let height
+//値の代入
+height=1.7
+//変数宣言
+let keisan
+//値の代入
+keisan='bmi'
+//BMI計算式
+console.log (bmi=(bodyWeght)/(height*height))
