d35b7cbe3a64285482790d6ba6aa95cbefa8fd48
大島　慶之 <160297931+jillsaw@users.noreply.github.com>
2024-08-25 21:41:51 +0900

    Lesson11_12&kadai_mdn_jsの追加

---
+++ Lesson11/object.js
@@ -0,0 +1,13 @@
+ // オブジェクトの宣言と値の代入を行う
+const personalDeta={name:'侍太郎',age:36,gender:'男性'};
+// オブジェクトの値を出力する
+console.log(personalDeta);
+// 'age'というキーの値を更新する
+personalDeta.age=37;
+// 新しくプロパティ（キーと値）を追加する
+personalDeta.address='東京都'
+//オブジェクトの値を出力する
+console.log(personalDeta);
+ // 'gender'というキーの値を出力する
+ console.log(personalDeta.gender);
+ 
---
+++ Lesson12/function.js
@@ -0,0 +1,13 @@
+const sayGoodMorning=()=>{
+  console.log('おはようございます！');
+  console.log('昨日はよく眠れましたか？');
+  console.log('今日も一日頑張りましょう！');
+}
+
+const sayGoodEvening=()=>{
+  console.log('こんばんは！');
+  console.log('今日も一日お疲れさまでした。');
+}
+
+sayGoodMorning();
+sayGoodEvening();
