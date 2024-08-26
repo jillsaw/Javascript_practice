482cf40bc83598c7ac2de853778f0173480d7d41
大島　慶之 <160297931+jillsaw@users.noreply.github.com>
2024-08-26 20:21:26 +0900

    Lesson13/14＆kadai_014の追加

---
+++ Lesson14/function.js
@@ -0,0 +1,40 @@
+const sayGoodMorning=()=>{
+  console.log('おはようございます！');
+  console.log('昨日はよく眠れましたか？');
+  console.log('今日も一日頑張りましょう！');
+}
+
+const sayGoodEvening=()=>{
+  console.log('こんばんは！');
+  console.log('今日も一日お疲れさまでした。');
+}
+
+sayGoodMorning();
+sayGoodEvening();
+
+const calculateTotal=(price)=>{
+  console.log(price+500+'円');
+}
+
+calculateTotal(1200);
+
+const addTowArguments=(price,shippingFee)=>{
+  console.log(price+shippingFee+'円');
+}
+
+addTowArguments(1200,500);
+
+const double=(num)=>{
+  return num*2;
+}
+
+console.log(double(30));
+
+const useVeriable=()=>{
+  const userName='侍太郎';
+  console.log(userName);
+}
+
+useVeriable();
+
+console.log(userName);
